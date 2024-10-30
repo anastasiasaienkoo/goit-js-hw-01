5689687abfcb691f92c27dfef4b88571cf77c0cf
Anastasia <n05012007@gmail.com>
2024-10-30 09:22:15 +0100

    update all

---
+++ js/task-1.js
@@ -0,0 +1,4 @@
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+ console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+}
---
+++ js/task-3.js
@@ -0,0 +1,5 @@
+function getElementWidth(content, padding, border) {
+  console.log(Number.parseFloat(content) +
+    Number.parseFloat(padding) * 2 +
+    Number.parseFloat(border) * 2);
+}
